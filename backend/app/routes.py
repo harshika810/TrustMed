9feccc63eb47648707310433cc978a6c435cfc81
mukulsharma421 <mukulsharma9988a@gmail.com>
2026-04-21 23:03:56 +0530

    updated ui with style and graph

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -11,8 +11,7 @@ def test_api():
 @router.get("/test-db")
 def test_db():
     health_collection.insert_one({"msg": "mongo working"})
-    return {"status": "MongoDB connected"}
-
+    return {"status": "MongoDB connected"}                                                                                                                                                                                       
 @router.post("/upload-health-data")
 def upload_health_data(data: HealthData):
     health_collection.insert_one(data.dict())
